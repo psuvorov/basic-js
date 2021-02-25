7cb8fd9b2d61c424eb65a2a8a93f884634572d64
PavelSuvorov <psuvorov@riverlogic.com>
2021-02-25 13:23:26 +0400

    Implement one more task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -6,10 +6,48 @@ module.exports = function repeater(str, options) {
 
 /**
  * 
- * @param {string} str
- * @param {{}} options
+ * @param {any} str
+ * @param {{
+ *     repeatTimes: number,
+ *     separator: string,
+ *     addition: string,
+ *     additionRepeatTimes: number,
+ *     additionSeparator: string
+ * }} options
  */
 function repeaterInt(str, options) {
+    if (!options.separator)
+        options.separator = '+';
+    if (!options.additionSeparator)
+        options.additionSeparator = '|';
     
+    let res = "";
+    if (!options.repeatTimes)
+        res = str;
+    if (!options.additionRepeatTimes && options.addition)
+        res += options.addition;
+    
+    for (let i = 1; i <= options.repeatTimes; i++) {
+        res += str;
+        
+        for (let j = 1; j <= options.additionRepeatTimes; j++) {
+            res += options.addition;
+            res += options.additionSeparator;
+        }
+        if (options.additionRepeatTimes)
+            res = res.substring(0, res.lastIndexOf(options.additionSeparator));
+        
+        res += options.separator;
+    }
+    
+    if (options.repeatTimes)
+        return res.substring(0, res.lastIndexOf(options.separator));
+    else
+        return res;
 }
 
+// let res = repeaterInt("STRING", { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' });
+// let res = repeaterInt("STRING", { repeatTimes: 0, separator: '**', addition: 'PLUS', additionRepeatTimes: 0, additionSeparator: '00' });
+
+// console.log(res);
+//"STRING PLUS00 PLUS00 PLUS**   STRING PLUS00 PLUS00 PLUS**   STRING PLUS00 PLUS00 PLUS";
